35d415f9e715890785a7998784b81b4756c902dc
foxy-dev1 <brownpandadev7@gmail.com>
2024-06-12 12:27:59 +0530

    modified the update forecast py and yml file

--- update_forecast.py
+++ update_forecast.py
@@ -7,74 +7,17 @@ from prophet import Prophet
 import os
 from github import Github
 
-# Load data
 filtered_df = pd.read_csv('data/filtered_df.csv')
 test_df = pd.read_csv('data/test_df.csv')
 combined_df = pd.concat([filtered_df, test_df], ignore_index=True)
 combined_df.rename(columns={'index': 'ds', 'pm25': 'y'}, inplace=True)
 
-# Train the Prophet model
 model = Prophet(interval_width=0.80, yearly_seasonality=True)
 model.fit(combined_df)
 
-# Make future predictions
 future = model.make_future_dataframe(periods=7)
 forecast_future = model.predict(future)
 
-# Get the last 10 lines of the forecast
-forecast_last_10 = forecast_future.tail(10)
+csv_filepath = '/home/runner/work/air-quality/air-quality/future_forecast.csv'
 
-# Convert the last 10 lines of the forecast to CSV
-forecast_csv = forecast_last_10.to_csv(index=False)
-
-# Authenticate to GitHub
-token = os.getenv('TOKEN_GITHUB')
-g = Github(token)
-
-# Check the rate limit
-rate_limit = g.get_rate_limit().core
-print(f"Remaining API requests: {rate_limit.remaining}")
-print(f"Rate limit resets at: {rate_limit.reset}")
-
-# If rate limit is exceeded, exit early
-if rate_limit.remaining == 0:
-    reset_time = (rate_limit.reset - datetime.utcnow()).total_seconds()
-    print(f"Rate limit exceeded. Waiting for {reset_time} seconds until reset.")
-    exit(1)
-
-# Define repository details
-repo_name = 'foxy-dev1/air-quality'
-file_path = 'home/runner/work/air-quality/air-quality/forecast.csv'  # Adjust this to the correct path in your repo
-commit_message = 'update forecast data'
-
-# Get the repository
-repo = g.get_repo(repo_name)
-
-# Function to get file contents with rate limit handling
-def get_file_contents(repo, file_path):
-    try:
-        contents = repo.get_contents(file_path)
-        print("File contents retrieved successfully.")
-        return contents
-    except Exception as e:
-        if e.status == 404:
-            print("File not found. Creating a new file.")
-            return None
-        else:
-            raise e
-
-# Get the file contents
-contents = get_file_contents(repo, file_path)
-
-# Create or update the file in the repository
-try:
-    if contents is None:
-        # Create the file if it doesn't exist
-        repo.create_file(file_path, commit_message, forecast_csv)
-        print("File created successfully.")
-    else:
-        # Update the file if it exists
-        repo.update_file(contents.path, commit_message, forecast_csv, contents.sha)
-        print("File updated successfully.")
-except Exception as e:
-    print(f"Error updating file: {e}")
+forecast_future.to_csv(csv_filepath,index=False)
